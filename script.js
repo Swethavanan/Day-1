2240810e312a94b08d425da521a39043f7fdb6ce
Swethavanan <swethavananme@gmail.com>
2024-08-03 21:56:35 +0530

    update

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 console.log("welcome to javascript")
+
+/*var a=10;
+a=20 ;//reassigne
+var a=30; //redeclare
+console.log(a);*/
+
+/*let a=30;
+a=40; //reassign
+console.log(a);*/
+
+/*const a=20
+a=30; //no reassign
+const=40; //no redeclare
+console.log(a);*/
+
+/* Window methods or events:-
+
+alert("welcome to google")
+
+prompt("enter your name")
+let result=prompt("enter your name");
+console.log(result);
+
+let result=confirm("this is the website?");
+console.log(result);*/
+
+/*undefined example
+let a;
+console.log(a);*/
+
+/*null example
+let num=null;
+console.log(num); //null
+console.log(typeof(num)); //object*/
+
+/*let person={
+    firstname:"swe",
+    lastname:"m",
+    age:24,
+    skill:["javascript","html","css"]
+}
+console.log(person);*/
